e126f1d976bb67671cf28de86b1d6f918484341c
FrankieFong-hk <httfong@gmail.com>
2025-06-11 16:33:23 -0400

    feat: configure CORS headers and update app metadata for deployment

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -5,10 +5,8 @@ import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
-  const publishableKey =
-    "pk_test_cGlja2VkLXR1cnRsZS04Mi5jbGVyay5hY2NvdW50cy5kZXYk";
   return (
-    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+    <ClerkProvider tokenCache={tokenCache}>
       <SafeScreen>
         <Slot />
       </SafeScreen>
